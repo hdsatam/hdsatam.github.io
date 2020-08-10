bd8256eb8c0ad504fffa234294bf281114ff1f08
Hershal Satam <hsatam@umich.edu>
2020-08-09 23:09:43 -0400

    Updates

---
+++ precache-manifest.61afd111c5a8e94d3cc275b810169671.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7dca4adffcc9eb8f8cc747e53222136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8cacf413a8ad90d796a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f23e9f035c60eb10bae",
+    "url": "/static/js/2.95d112ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95d112ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8cacf413a8ad90d796a",
+    "url": "/static/js/main.66dd622d.chunk.js"
+  },
+  {
+    "revision": "4e23075430d083c7aedb",
+    "url": "/static/js/runtime-main.7dc27a67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
